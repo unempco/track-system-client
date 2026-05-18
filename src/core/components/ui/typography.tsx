4eebd11774ddbf35a325f313a9468478efde64e1
Paul2G <pggaleana@gmail.com>
2026-05-18 13:10:14 -0700

    feat(core): remove ugly padding from typography h2

--- src/core/components/ui/typography.tsx
+++ src/core/components/ui/typography.tsx
@@ -21,7 +21,7 @@ const typographyVariants = cva('', {
   variants: {
     variant: {
       h1: 'scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance',
-      h2: 'scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+      h2: 'scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0',
       h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
       h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
       p: 'leading-7 [&:not(:first-child)]:mt-6',
